1ab3f977f4e971ef9fff3c523e6091f32a11bb9c
Drew Harry <drew.harry@gmail.com>
2026-07-19 21:43:22 -0700

    Make player big.

--- modules/game/actors/player.lua
+++ modules/game/actors/player.lua
@@ -1,9 +1,9 @@
 prism.registerActor("Player", function()
    return prism.Actor.fromComponents {
       prism.components.Name("Player"),
-      prism.components.Drawable { index = "@", color = prism.Color4.BLUE },
+      prism.components.Drawable { index = "@", color = prism.Color4.BLUE, size = 4 },
       prism.components.Position(),
-      prism.components.Collider(),
+      prism.components.Collider { allowedMovetypes = { "walk" }, size = 4 },
       prism.components.PlayerController(),
       prism.components.Senses(),
       prism.components.Sight { range = 64, fov = true },
